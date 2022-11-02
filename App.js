faeeae16fdee1ebba22aabdc4fba95fe13a6ba30
Hung <daminhnguyenhung@gmail.com>
2022-11-02 12:17:30 +0700

    done lesson 1

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useRef, useState } from "react";
+import { Animated, Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const fadeAnimation = useRef(new Animated.Value(0)).current;
+
+    const fadeIn = () => {
+        Animated.timing(fadeAnimation, {
+            toValue: 1,
+            duration: 5000,
+            useNativeDriver: true,
+        }).start();
+    };
+
+    useEffect(() => {
+        fadeIn();
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <Animated.View style={[{ opacity: fadeAnimation }]}>
+                <Text>Welcome to Animation React Native</Text>
+                <StatusBar style="auto" />
+            </Animated.View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
